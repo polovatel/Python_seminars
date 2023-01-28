86369216257ef3067db4e1ccb30db9a7a12d5837
Blyatko_Blyatich <vissar2016@gmail.com>
2023-01-29 02:24:55 +0300

    Initial comit for seminar 7

---
+++ Seminar_7/contact_details.py
@@ -0,0 +1,11 @@
+def surname_of_contact():
+    return input('Input surname: ')
+    
+def name_of_contact():
+    return input('Input surname: ')
+    
+def phone_number():
+    return int(input('Input phone number: '))
+
+def description():
+    return input('Input description: ')
